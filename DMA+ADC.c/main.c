c1c15df3541403150f04c85865e79b09d17a4332
HaHaHaHao3180 <3447664126@qq.com>
2025-04-21 10:15:53 +0800

    ADD DMA+ADC

---
+++ DMA+ADC.c/main.c
@@ -0,0 +1,24 @@
+#include "stm32f10x.h"// Device header
+#include"Delay.h"
+#include"OLED.h"
+#include"AD.h"
+
+
+int main(void)
+ {
+	 OLED_Init();
+	 AD_Init();
+	 
+	 OLED_ShowString(1,1,"AD1:");
+	 OLED_ShowString(2,1,"AD2:");
+	 OLED_ShowString(3,1,"AD3:");
+
+	 
+	while(1)
+	 {
+		OLED_ShowNum(1, 5, AD_Value[0], 4);
+		OLED_ShowNum(2, 5, AD_Value[1], 4);
+		OLED_ShowNum(3, 5, AD_Value[2], 4);
+        Delay_ms(200);
+	 }
+ }
